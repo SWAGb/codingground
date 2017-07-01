4da774691c01463ed9a9e49e7c1631d37cf62b20
SWAGATA BHATTACHARYYA <sbskfgi@gmail.com>
2017-07-02 05:24:17 +0530

    Created by Coding Ground Client

---
+++ New Project-20170701/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main()
+{
+    
+    int a=10;
+    float b=12.3;
+    
+    char  c;
+    int i=11;
+  i=c;
+    int m=a+(int)b;
+    float n=(float)a+b;
+    printf("%d\n",m);
+    printf("%f\n",n);
+    printf("%d\n",i);
+}
+
+
+
